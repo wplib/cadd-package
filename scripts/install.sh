c5a4a0970b68d9306b7e2a693966e44e86fa2b32
Daryl Lozupone <daryl@newclarity.net>
2017-05-08 15:32:56 -0400

    Add the box CLI command to switch to Caddy web server in box

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
+# Create the docker container
 docker create -p 80:80 --name caddy -v /var/www:/var/www wplib/caddy
+
+# Add the command to set the box to use the caddy server
+cp ../commands/caddy /box/scripts/guest/cli/commands/caddy
